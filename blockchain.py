0141007d2bd932c4807ced08e43a705bd5be2470
kingalu14 <kamiraki@gmail.com>
2019-10-06 02:10:23 +0300

    --- Auto-Git Commit ---

--- blockchain.py
+++ blockchain.py
@@ -40,16 +40,19 @@ def print_blockchain_elements():
     for block in blockchain:
         print('Outputting Block')
         print(block)
+    else:
+        print('-' * 20)
 
 def verify_chain():
-    block_index = 0
+    # block_index = 0
     is_valid=True
-    for block in blockchain:
-        if block[0] == blockchain[block_index - 1]:
+    for block_index in range(len(blockchain)):
+        if block_index == 0:
+            continue
+        elif blockchain[block_index][0] == blockchain[block_index - 1]:
             is_valid = True
         else:
             is_valid = False   
-            break 
     return is_valid
  
 
@@ -60,7 +63,8 @@ tx_amount = get_transaction_value()
 add_value(tx_amount)
 
 """
-while True:
+waiting_for_input = True
+while waiting_for_input:
     print('Please choose')
     print('1:Add a new transaction value')
     print('2:Output the blockchain blocks')
@@ -75,9 +79,14 @@ while True:
          print_blockchain_elements()
     elif user_choice=="h":
         if len(blockchain) >= 1:
-            blockchain[0]=2    
+            blockchain[0]=[2]    
     elif user_choice=="q":  
-         break   
+        waiting_for_input = False 
     else:
         print('İnput Was invalid, please pick the value from the list!')
+    if not verify_chain():
+          print('İnvalid blockchain')
+          break
+else:
+    print('User Left')
 print('Done!')
