102925594f8bca5a1eaf62e6985661cf93d01e0e
kingalu14 <kamiraki@gmail.com>
2019-10-16 17:56:03 +0300

    --- Auto-Git Commit ---

--- blockchain.py
+++ blockchain.py
@@ -1,107 +1,126 @@
 # Initializing our (empty) blockchain list
 blockchain = []
-open_transaction = []
+open_transactions = []
+owner = 'Max'
 
 
 def get_last_blockchain_value():
     """ Returns the last value of the current blockchain. """
-    if len(blockchain) < 1 :
-       return None
+    if len(blockchain) < 1:
+        return None
     return blockchain[-1]
 
 # This function accepts two arguments.
 # One required one (transaction_amount) and one optional one (last_transaction)
 # The optional one is optional because it has a default value => [1]
 
-def mine_block():
-    pass
 
-
-def add_value(transaction_amount, last_transaction=[1]):
-    """ 
-    Append a new value as well as the last blockchain value to the blockchain.
+def add_transaction(recipient, sender=owner, amount=1.0):
+    """ Append a new value as well as the last blockchain value to the blockchain.
 
     Arguments:
-        :transaction_amount: The amount that should be added.
-        :last_transaction: The last blockchain transaction (default [1]).
+        :sender: The sender of the coins.
+        :recipient: The recipient of the coins.
+        :amount: The amount of coins sent with the transaction (default = 1.0)
     """
-    if last_transaction == None:
-        last_transaction = [1]
-    blockchain.append([last_transaction, transaction_amount])
+    transaction = {
+        'sender': sender,
+        'recipient': recipient,
+        'amount': amount
+    }
+    open_transactions.append(transaction)
+
+
+def mine_block():
+    pass
 
 
 def get_transaction_value():
     """ Returns the input of the user (a new transaction amount) as a float. """
     # Get the user input, transform it from a string to a float and store it in user_input
-    user_input = float(input('Your transaction amount please: '))
-    return user_input
+    tx_recipient = input('Enter the recipient of the transaction: ')
+    tx_amount = float(input('Your transaction amount please: '))
+    return tx_recipient, tx_amount
+
 
 def get_user_choice():
-    user_input = input("Your Choice: ")  
-    return user_input  
+    """Prompts the user for its choice and return it."""
+    user_input = input('Your choice: ')
+    return user_input
+
 
 def print_blockchain_elements():
+    """ Output all blocks of the blockchain. """
+    # Output the blockchain list to the console
     for block in blockchain:
         print('Outputting Block')
         print(block)
     else:
         print('-' * 20)
 
+
 def verify_chain():
-    """    
-    block_index = 0
-    is_valid=True
-    for block in blockchain:
+    """ Verify the current blockchain and return True if it's valid, False otherwise."""
+    # block_index = 0
+    is_valid = True
+    for block_index in range(len(blockchain)):
         if block_index == 0:
-            block_index += 1
+            # If we're checking the first block, we should skip the iteration (since there's no previous block)
             continue
-        elif block[0] == blockchain[block_index - 1]:
+        # Check the previous block (the entire one) vs the first element of the current block
+        elif blockchain[block_index][0] == blockchain[block_index - 1]:
             is_valid = True
-            block_index += 1
         else:
-            is_valid = False   
-            break 
+            is_valid = False
+    #         break
+    # for block in blockchain:
+    #     if block_index == 0:
+    #         block_index += 1
+    #         continue
+    #     elif block[0] == blockchain[block_index - 1]:
+    #         is_valid = True
+    #     else:
+    #         is_valid = False
+    #         break
+    #     block_index += 1
     return is_valid
-    """
-    for(index,block) in enumurate(blockchain):
-        if index == 0:
-            continue
-        if block['previous_hash'] == hash_block(blockchain[index-1]):
-            return False
-    return True
- 
 
 
-# Get the first transaction input and add the value to the blockchain
-"""
-tx_amount = get_transaction_value()
-add_value(tx_amount)
-
-"""
 waiting_for_input = True
+
+# A while loop for the user input interface
+# It's a loop that exits once waiting_for_input becomes False or when break is called
 while waiting_for_input:
     print('Please choose')
-    print('1:Add a new transaction value')
-    print('2:Output the blockchain blocks')
-    print('h:Manupulate the blockchain')
-    print('q:Quit')
+    print('1: Add a new transaction value')
+    print('2: Output the blockchain blocks')
+    print('h: Manipulate the chain')
+    print('q: Quit')
     user_choice = get_user_choice()
-    if  user_choice=="1":
-        tx_amount = get_transaction_value()
-        add_value(tx_amount, get_last_blockchain_value())
-    elif user_choice=="2":
-    # Output the blockchain list to the console
-         print_blockchain_elements()
-    elif user_choice=="h":
+    if user_choice == '1':
+        tx_data = get_transaction_value()
+        recipient, amount = tx_data
+        # Add the transaction amount to the blockchain
+        add_transaction(recipient, amount=amount)
+        print(open_transactions)
+    elif user_choice == '2':
+        print_blockchain_elements()
+    elif user_choice == 'h':
+        # Make sure that you don't try to "hack" the blockchain if it's empty
         if len(blockchain) >= 1:
-            blockchain[0]=[2]    
-    elif user_choice=="q":  
-        waiting_for_input = False 
+            blockchain[0] = [2]
+    elif user_choice == 'q':
+        # This will lead to the loop to exist because it's running condition becomes False
+        waiting_for_input = False
     else:
-        print('İnput Was invalid, please pick the value from the list!')
+        print('Input was invalid, please pick a value from the list!')
     if not verify_chain():
-          print('İnvalid blockchain')
-          break
+        print_blockchain_elements()
+        print('Invalid blockchain!')
+        # Break out of the loop
+        break
 else:
-    print('User Left')
+    print('User left!')
+
+
 print('Done!')
