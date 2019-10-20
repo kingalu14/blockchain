5641f722aae549e011d5592e90136a379e1d36da
kingalu14 <kamiraki@gmail.com>
2019-10-21 02:00:45 +0300

    --- Auto-Git Commit ---

--- assignment2.py
+++ assignment2.py
@@ -2,8 +2,11 @@ person = [{'name':'kassim','age':30,'hobby':['Movie','Traveling']},{'name':'Ufuk
 print(person[0]['hobby'])
 person_names = [el['name'] for el in person]
 print(person_names)
-all_person_older_than_20 = any([el['age'] > 20  for el in person ])
+all_person_older_than_20 = all([el['age'] > 20  for el in person ])
 print(all_person_older_than_20)
 person2 = person[:]
 person2[0]['name']="Poul"
 print(person2)
+[a,b]=person
+print(a)
+print(b)
